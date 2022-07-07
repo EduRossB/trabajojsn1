03c0c1770ee6e7507494c88bebc7ac08f99e32d5
EduRossB <edu_barba@outlook.com>
2022-07-06 22:04:50 -0300

    ejercicio nº7

--- js/ejnº7.js
+++ js/ejnº7.js
@@ -2,3 +2,15 @@
 let numero1 = parseInt(prompt("Ingresar el primer numero"))
 let numero2 = parseInt(prompt("Ingresar el segundo numero"))
 let numero3 = parseInt(prompt("Ingresar el tercer numero"))
+
+if(numero1 > numero2 && numero1 >numero3)
+{
+    document.write("El numero mayor es: " + numero1)
+}
+else if(numero2 > numero3)
+{
+    document.write("El numero mayor es: " + numero2)
+}
+else{
+    document.write("El numero mayor es: " + numero3)
+}
